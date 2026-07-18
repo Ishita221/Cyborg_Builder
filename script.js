f8cdc66e6ddd73a1a7c594d280a145f3f65aaf37
Ishita221 <ishitasrimali@gmail.com>
2026-07-18 15:17:12 +0530

    cyborg head  color changes with qualities

--- script.js
+++ script.js
@@ -12,4 +12,18 @@ buildbtn.addEventListener("click", function () {
 
   const output = document.getElementById("output");
   output.textContent = "Selected: " + qualities.join(", ");
+
+let headColor = "gray";
+  if (qualities.includes("brave")) headColor = "red";
+  if (qualities.includes("cold")) headColor = "blue";
+
+
+  const cyborg = document.getElementById("cyborg");
+  cyborg.innerHTML = `
+    <svg width="200" height="200">
+      <rect x="50" y="50" width="100" height="100" fill="${headColor}" />
+      <circle cx="80" cy="90" r="10" fill="cyan" />
+      <circle cx="120" cy="90" r="10" fill="cyan" />
+    </svg>
+  `;
 });
